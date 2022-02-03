2ae90a1d1eb2b6e2c3e95cb010097f4b699d5f9f
Adam Taylor <afranklint16@gmail.com>
2022-02-02 18:34:42 -0700

    Reverted some earlier changes

--- src/main/java/com/github/pocketkid2/atmosphere/AtmosphereCommand.java
+++ src/main/java/com/github/pocketkid2/atmosphere/AtmosphereCommand.java
@@ -1,11 +1,6 @@
 package com.github.pocketkid2.atmosphere;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
 import org.bukkit.ChatColor;
-import org.bukkit.Material;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -21,12 +16,10 @@ public class AtmosphereCommand implements CommandExecutor {
 
 	@Override
 	public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-		if (!(sender instanceof Player)) {
+		if (!(sender instanceof Player player)) {
 			sender.sendMessage(ChatColor.RED + "You must be a player!");
 			return true;
 		}
-		Player player = (Player) sender;
-
 		if (args.length < 1) {
 			player.sendMessage(ChatColor.RED + "Not enough arguments!");
 			return false;
@@ -35,18 +28,8 @@ public class AtmosphereCommand implements CommandExecutor {
 		if (args[0].equalsIgnoreCase("test")) {
 			plugin.toggleTest(player);
 			player.sendMessage(ChatColor.AQUA + "Atmosphere testing has been turned " + (plugin.isTesting(player) ? ChatColor.GREEN + "on" : ChatColor.RED + "off"));
-			return true;
 		}
-		if (args[0].equalsIgnoreCase("list")) {
-			List<Material> list = new ArrayList<Material>(Arrays.asList(Material.values()));
-			for (Material m : list) {
-				player.sendMessage(ChatColor.AQUA + m.toString());
-			}
-			return true;
-		} else {
-			player.sendMessage(ChatColor.RED + "Unknown argument " + ChatColor.GRAY + args[0]);
-			return false;
-		}
+		return true;
 	}
 
 }
